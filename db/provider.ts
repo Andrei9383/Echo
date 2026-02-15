baf4b9c1b4b4c127a556635ec6ba005f5c244cb7
Andrei9383 <andreistan275@gmail.com>
2026-02-15 03:42:55 +0200

    impl: singleton for the database

--- db/provider.ts
+++ db/provider.ts
@@ -1,15 +1,25 @@
-import { drizzle } from "drizzle-orm/expo-sqlite";
-import { openDatabaseSync, useSQLiteContext } from "expo-sqlite";
+import { drizzle, ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
+import {
+  openDatabaseSync,
+  useSQLiteContext,
+  SQLiteDatabase,
+} from "expo-sqlite";
 import { useMemo } from "react";
 
 export const DATABASE_NAME = "activities";
 
+let db: ExpoSQLiteDatabase | null = null;
+
 export const createDb = () => {
-	const expoDb = openDatabaseSync(DATABASE_NAME);
-	return drizzle(expoDb);
+  if (!db) {
+    const expoDb = openDatabaseSync(DATABASE_NAME);
+    db = drizzle(expoDb);
+  }
+
+  return db;
 };
 
 export const useDb = () => {
-	const expoDb = useSQLiteContext();
-	return useMemo(() => drizzle(expoDb), [expoDb]);
+  const expoDb = useSQLiteContext();
+  return useMemo(() => drizzle(expoDb), [expoDb]);
 };
